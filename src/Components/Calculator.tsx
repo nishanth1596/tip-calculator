028ce7e094b29072251742d81ee2ba9480f1d003
Nishanth <nishanth1596@gmail.com>
2025-02-01 01:30:41 +0530

    fixed border-radius for large devices

--- src/Components/Calculator.tsx
+++ src/Components/Calculator.tsx
@@ -3,7 +3,7 @@ import Display from "./Display";
 
 const Calculator = () => {
   return (
-    <article className="rounded-t-3xl bg-white pb-8 drop-shadow-[0_35px_35px_rgba(0,0,0,0.2)] md:grid md:grid-cols-2 md:gap-12 md:pb-0">
+    <article className="rounded-t-3xl bg-white pb-8 drop-shadow-[0_35px_35px_rgba(0,0,0,0.2)] md:grid md:grid-cols-2 md:gap-12 md:rounded-3xl md:pb-0">
       <CalculationInput />
       <Display />
     </article>
